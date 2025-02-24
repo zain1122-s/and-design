8c709eeff7da00964e2e62f6a4541ad789614692
shahzain alam <shahzaina354@gmail.com>
2025-02-24 21:15:43 +0000

    chng

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import {  Routes , Route} from 'react-router'
+import Ant from './ant.jsx';
+function App() {
+
+
+  return (
+  <>
+  {/* <Routes>
+    <Route />
+  </Routes> */}
+
+  <Ant/>
+  </>
+
+  )
+};
+
+export default App
