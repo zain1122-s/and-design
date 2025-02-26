862e8970f17c87d2f31751fe4a3e20fa21446a04
shahzain alam <shahzaina354@gmail.com>
2025-02-26 22:59:02 +0000

    chng

--- src/component/card/card.jsx
+++ src/component/card/card.jsx
@@ -5,45 +5,45 @@ const { Meta } = Card;
 export default function Cards() {
   return (
     <div>
-  <Row >
-    <Col span={24} sm={6} >
+  <Row>
+    <Col span={24} sm={6} offset={1} >
     <Card
     hoverable
     style={{
-      width: 240,
+      width: 300,
     }}
     cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
   >
     <Meta title="Europe Street beat" description="www.instagram.com" />
   </Card>
     </Col>
-    <Col span={24} sm={6} >
+    <Col span={24} sm={6} offset={1}>
     <Card
     hoverable
     style={{
-      width: 240,
+      width: 300,
     }}
     cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
   >
     <Meta title="Europe Street beat" description="www.instagram.com" />
   </Card>
     </Col>
-    <Col span={24} sm={6} >
+    <Col span={24} sm={6} offset={1}>
     <Card
     hoverable
     style={{
-      width: 240,
+      width: 300,
     }}
     cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
   >
     <Meta title="Europe Street beat" description="www.instagram.com" />
   </Card>
     </Col>
-    <Col span={24} sm={6} >
+    <Col span={24} sm={6} offset={1}>
     <Card
     hoverable
     style={{
-      width: 240,
+      width: 300,
     }}
     cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
   >
--- src/container/homePage/index.jsx
+++ src/container/homePage/index.jsx
@@ -1,9 +1,11 @@
 import MainLayout from "../Layout";
 import Cards from "../../component/card/card";
+import Heading from "../../component/typography/typography";
 function HomePage() {
   return (
     <MainLayout>
       <Cards/>
+      <Heading/>
     </MainLayout>
   );
 }
